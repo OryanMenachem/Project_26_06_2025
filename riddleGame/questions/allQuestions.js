7b1fe06b130b9396db9924dec4b0fbc4d7025343
Oryan Menachem <om3160204@gmail.com>
2025-06-26 12:59:25 +0300

    Adding a file that imports all the questions, and adding a Gitignore code

---
+++ riddleGame/questions/allQuestions.js
@@ -0,0 +1,44 @@
+import {question1} from './question1.js'
+import {question2} from './question2.js'
+import {question3} from './question3.js'
+import {question4} from './question4.js'
+import {question5} from './question5.js'
+import {question6} from './question6.js'
+import {question7} from './question7.js'
+import {question8} from './question8.js'
+import {question9} from './question9.js'
+import {question10} from './question10.js'
+import {question11} from './question11.js'
+import {question12} from './question12.js'
+import {question13} from './question13.js'
+import {question14} from './question14.js'
+import {question15} from './question15.js'
+import {question16} from './question16.js'
+import {question17} from './question17.js'
+import {question18} from './question18.js'
+import {question19} from './question19.js'
+import {question20} from './question20.js'
+
+const question1 = question1;
+const question2 = question2;
+const question3 = question3;
+const question4 = question4;
+const question5 = question5;
+const question6 = question6;
+const question7 = question7;
+const question8 = question8;
+const question9 = question9;
+const question10 = question10;
+const question11= question11;
+const question12 = question12;
+const question13 = question13;
+const question14 = question14;
+const question15 = question15;
+const question16 = question16;
+const question17 = question17;
+const question18 = question18;
+const question19 = question19;
+const question20 = question20;
+
+
+
