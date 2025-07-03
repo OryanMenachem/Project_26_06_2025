6cb2681b591eac6a4ff0ffd7a6990f349f21f034
Oryan Menachem <om3160204@gmail.com>
2025-07-03 16:30:58 +0300

    Creating 'inputRiddle.js' with functions to receive a riddle object from the user

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-import * as messages from './RiddleGame/UI/consolePrinting.js';
-import riddlesSet from './RiddleGame/Riddles/importRiddles .js';
-import {inputPlayerName, inputDifficultyLevel} from './RiddleGame/Player/playerInputFunc.js';
-import {Player} from './RiddleGame/Player/playerClass.js';
-import {askAllRiddles} from './RiddleGame/Riddles/askAllRiddlesFunc.js'
+// import * as messages from './RiddleGame/UI/consolePrinting.js';
+// import riddlesSet from './RiddleGame/Riddles/importRiddles .js';
+// import {inputPlayerName, inputDifficultyLevel} from './RiddleGame/Player/playerInputFunc.js';
+// import {Player} from './RiddleGame/Player/playerClass.js';
+// import {askAllRiddles} from './RiddleGame/Riddles/askAllRiddlesFunc.js'
+import input from './inputRiddleObj.js';
+import write from './RiddleGame/modules/create.js'
+
+const dbPath = "C:/Users/om316/OneDrive/Desktop/JavaScript/Projects/Project_26_06_2025/DB/riddlesDB.txt"
+write(dbPath,input());
 
 
 
+// // Prints a greeting message to the console.
+// console.log(messages.sayWelcome());  
 
 
-
-// Prints a greeting message to the console.
-console.log(messages.sayWelcome());  
-
-
-// Prints a general description of the game to the console.
-console.log(messages.displayInstructions());  
+// // Prints a general description of the game to the console.
+// console.log(messages.displayInstructions());  
 
 
-//  Asks the user for their name and stores it in a variable.
-const playerName = inputPlayerName();  
+// //  Asks the user for their name and stores it in a variable.
+// const playerName = inputPlayerName();  
 
 
-// Prints a "Hello" message to the user in the console.
-console.log(messages.sayHelloPlayer(playerName));  
+// // Prints a "Hello" message to the user in the console.
+// console.log(messages.sayHelloPlayer(playerName));  
 
 
-// Creates a Player instance with the user's name.
-const player1 = new Player(playerName);  
+// // Creates a Player instance with the user's name.
+// const player1 = new Player(playerName);  
 
 
-// Shows the user difficulty levels to choose from, and stores the user's choice in a variable.
-const difficultyLevel = inputDifficultyLevel();  
+// // Shows the user difficulty levels to choose from, and stores the user's choice in a variable.
+// const difficultyLevel = inputDifficultyLevel();  
 
-// Loads questions into an array based on the difficulty the user chose.
-const arrayOfQuestions = riddlesSet.filter((element => element.difficulty == difficultyLevel))  
-
-
-// Asks all questions based on the chosen difficulty.
-askAllRiddles(arrayOfQuestions, player1);   
+// // Loads questions into an array based on the difficulty the user chose.
+// const arrayOfQuestions = riddlesSet.filter((element => element.difficulty == difficultyLevel))  
 
 
-// Prints a congratulation message to the console for winning the game.
-messages.DisplaySuccessMessage();  
+// // Asks all questions based on the chosen difficulty.
+// askAllRiddles(arrayOfQuestions, player1);   
 
 
-// Prints to the console the total game time and the average time the user took to answer each question.
-player1.showStats();  
+// // Prints a congratulation message to the console for winning the game.
+// messages.DisplaySuccessMessage();  
+
+
+// // Prints to the console the total game time and the average time the user took to answer each question.
+// player1.showStats();  
 
 
 
