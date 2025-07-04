0dab02323e5de0596c362e238e2eda532aa86e50
Oryan Menachem <om3160204@gmail.com>
2025-07-04 10:29:38 +0300

    End of 'delete.js' implementation

--- RiddleGame/modules/delete.js
+++ RiddleGame/modules/delete.js
@@ -1,10 +1,26 @@
+import readline from 'readline-sync';
 import * as fsp from 'node:fs/promises';
 import dbContents from './read.js';
 import * as colors from '../UI/colors.js';
 
 
+function inputId() {
 
-async function deleteObj(dbContent) {
+     let id;
+    
+     while(true) {
+    
+        console.log(`\nEnter the id of object:`);
+        id = readline.question(`> `)
+    
+        if (id) {return id}
+        
+        console.log(colors.errorColor(`\nNo id entered!\n`));
+        
+        }
+}
+
+function deleteObj(dbContent, id) {
      
     for (let i = 0; i < dbContent.length; i++) {
     
@@ -17,6 +33,7 @@ async function deleteObj(dbContent) {
 
     }
     console.log(colors.errorColor('No object with such id exists!'));
+    return false;
 }
     
 
@@ -42,9 +59,15 @@ export default async function deleteManager(dbPath) {
 
     dbContent = JSON.parse(dbContent)
 
-    if (!deleteObj(dbContent)) {return}
+    const id = inputId();
+    
+    if (!deleteObj(dbContent, id)) {return}
     
     updateDB(dbContent);
 
 }
 
+
+const dbPath = "C:/Users/om316/OneDrive/Desktop/JavaScript/Projects/Project_26_06_2025/DB/riddlesDB.txt"
+
+deleteManager(dbPath);
